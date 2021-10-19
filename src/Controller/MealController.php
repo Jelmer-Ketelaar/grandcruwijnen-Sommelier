26ecf83380e69dedee1c0cc8675739b7f7489aca
JurrijnModderman <99053654@mydavinci.nl>
2021-10-19 15:36:36 +0200

    Added function to get everything from the mealMatcher

--- src/Controller/MealController.php
+++ src/Controller/MealController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\MealMatcher;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class MealController extends AbstractController
 {
     #[Route('/', name: 'meal')]
-    public function index(): Response
-    {
-        return $this->render('meal/index.html.twig', [
-            'controller_name' => 'MealController',
-        ]);
+    public function index(Request $request): Response {
+        //get everything from mealMatcher
+        $mealMatcher = $this->getDoctrine()->getRepository(MealMatcher::class)->findAll();
+        return $this->json($mealMatcher);
+    }
+
+    public function create(Request $request): Response {
+        return $this->json('');
     }
 }
