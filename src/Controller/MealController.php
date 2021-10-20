10ab24020c3bc5ea1a08b4ab735ab1afc6af5fcd
Jelmer <jelmer@ketelaar.me>
2021-10-20 12:27:50 +0200

    Added the categories and the meals
    I added al the categories and subcategories and the meals. I also made them clickable

--- src/Controller/MealController.php
+++ src/Controller/MealController.php
@@ -2,22 +2,32 @@
 
 namespace App\Controller;
 
-use App\Entity\MealMatcher;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;
 
-class MealController extends AbstractController
-{
-    #[Route('/', name: 'meal')]
-    public function index(Request $request): Response {
-        //get everything from mealMatcher
-        $mealMatcher = $this->getDoctrine()->getRepository(MealMatcher::class)->findAll();
-        return $this->json($mealMatcher);
+use App\Service\MealMatcherService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class MealController extends AbstractController {
+    /**
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    #[Route('/', name: 'meal_categories')]
+    public function getMealCategories(MealMatcherService $mealMatcherService): Response
+    {
+        return $this->render('categories/index.html.twig', ['meals' => $mealMatcherService->getParentMealCategories()]);
     }
 
-    public function create(Request $request): Response {
-        return $this->json('');
+    #[Route('/{parentId}', name: 'meal_categories_for_parent')]
+    public function getCategoriesForParent(int $parentId, MealMatcherService $mealMatcherService)
+    {
+        return $this->render('categories/index.html.twig', ['meals' => $mealMatcherService->getCategoriesForParent($parentId)]);
+    }
+
+    #[Route('/meals/{categoryId}', name: 'meals_for_category')]
+    public function getMealsForCategory(int $categoryId, MealMatcherService $mealMatcherService)
+    {
+        return $this->render('meals/index.html.twig', ['meals' => $mealMatcherService->getMealsForCategory($categoryId)]);
     }
 }
