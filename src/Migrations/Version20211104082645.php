b17bdc47894987a745ddc4fb4f8ccc00d9a87d1b
Kawian <99052760@mydavinci.nl>
2021-11-09 09:19:03 +0100

    env file

--- src/Migrations/Version20211104082645.php
+++ src/Migrations/Version20211104082645.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20211103083317 extends AbstractMigration
+final class Version20211104082645 extends AbstractMigration
 {
     public function getDescription(): string
     {
