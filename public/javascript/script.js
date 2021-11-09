e683e47b40f3e716ba14a7f3b7854502a2bee753
Kingston <kingston@vanlaak.cc>
2021-11-09 09:04:20 +0100

    back to function created and added in twig

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,3 +1,17 @@
-function goBack() {
-    window.history.back();
+var baseUrl = 'http://127.0.0.1:8000/';
+
+function goBack(currentUrl) {
+
+    if(currentUrl.includes("/categories")){
+        window.location.replace(baseUrl);
+    }
+
+    if(currentUrl.includes("/category/")){
+        window.location.replace(baseUrl);
+    }
+    
+
+
+
+    //document.cookie = "username=John Doe";
 }
