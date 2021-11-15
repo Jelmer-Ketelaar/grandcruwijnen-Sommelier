911e7c0a8859144486d9cfd59240d3f6e20cfc2a
Jelmer Ketelaar <jelmer@ketelaar.me>
2021-11-15 23:36:00 +0100

    Fixed filter function

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -2,56 +2,19 @@ var baseUrl = 'http://127.0.0.1:8000/';
 
 function goBack(currentUrl) {
 
-    if(currentUrl.includes("/categories")){
+    if (currentUrl.includes("/categories")) {
         window.location.replace(baseUrl);
     }
 
-    if(currentUrl.includes("/category/")){
-        window.location.replace(baseUrl+"categories");
+    if (currentUrl.includes("/category/")) {
+        window.location.replace(baseUrl + "categories");
     }
-    
-    if(currentUrl.includes("/meals/")){
+
+    if (currentUrl.includes("/meals/")) {
         window.location.replace(localStorage.getItem('categoryId'));
     }
 
-    if(currentUrl.includes("/matches/")){
+    if (currentUrl.includes("/matches/")) {
         window.location.replace(localStorage.getItem('mealId'));
     }
 }
-
-function submitWineFilter(){
-    var delayInMilliseconds = 1000; //1 second
-
-setTimeout(function() {
-    window.location.reload()
-}, delayInMilliseconds);
-    
-}
-// ----------------------- price filter -------------------
-
-$(function () {
-    $('#producten').slider({
-        range: true,
-        min: 0,
-        max: 5000,
-        values: [0, 5000],
-        create: function() {
-            $("#amount").val("€0 - €5000");
-        },
-        slide: function (event, ui) {
-            $("#amount").val("€" + ui.values[0] + " - €" + ui.values[1]);
-            var mi = ui.values[0];
-            var mx = ui.values[1];
-            filterSystem(mi, mx);
-        }
-    })
-});
-
-function filterSystem(minPrice, maxPrice) {
-    $("#producten").hide().filter(function () {
-        var price = parseInt($(this).data("price"), 10);
-        return price >= minPrice && price <= maxPrice;
-    }).show();
-}
-
-//   --------------------- end price filter -----------------
--- src/Controller/MealController.php
+++ src/Controller/MealController.php
@@ -7,12 +7,20 @@ use App\Repository\ProductRepository;
 use App\Service\MealMatcherService;
 use GuzzleHttp\Exception\GuzzleException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 class MealController extends AbstractController
 {
+
+    private $requestStack;
+
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+    }
 
     /**
      * @throws GuzzleException
@@ -50,42 +58,34 @@ class MealController extends AbstractController
         return $this->render('meals/index.html.twig', ['meals' => $mealMatcherService->getMealsForCategory($categoryId)]);
     }
 
-    private $requestStack;
-
-    public function __construct(RequestStack $requestStack)
-    {
-        $this->requestStack = $requestStack;
-    }
-
     /**
      * @throws GuzzleException
      */
     #[Route('matches/{mealId}', name: 'wines_for_meals')]
-    public function getWinesForMeals(Request $request, ProductRepository $products, $mealId,  MealMatcherService $mealMatcherService): Response
+    public function getWinesForMeals(Request $request, ProductRepository $products, $mealId, MealMatcherService $mealMatcherService): Response
     {
         $matches = [];
 //        dd($mealMatcherService->getWinesForMeal($mealId));
-        $price_min = 10000;
-        $price_max = 5;
 
         $session = $this->requestStack->getSession();
         $session->set('mealId', array('mealId' => $mealId));
 
         $formMinPrice = $request->query->get('price-min');
+        if ($formMinPrice === null) {
+            $formMinPrice = 0;
+        }
         $formMaxPrice = $request->query->get('price-max');
+        if ($formMaxPrice === null) {
+            $formMaxPrice = 5000;
+        }
 
 
         foreach ($mealMatcherService->getWinesForMeal($mealId) as $wine) {
-            $product = $products->findOneBy(['sku' => $wine->wine->sku]);
+            $product = $products->findBySkuAndPrice($wine->wine->sku, $formMinPrice, $formMaxPrice);
             if ($product !== null) {
                 $score = $wine->score;
                 $productMatch = new ProductMatch($product, $score);
-
                 $matches[] = $productMatch;
-
-//                $score'' = $product;
-
-                //TODO: Scores toevoegen aan de wijn
             }
         }
 
