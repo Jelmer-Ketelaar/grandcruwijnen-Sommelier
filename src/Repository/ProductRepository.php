05d13f25ad14aad00c427301765d01522af54773
Jelmer <jelmer@ketelaar.me>
2021-11-18 14:14:49 +0100

    Update ProductRepository.php

--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -19,6 +19,11 @@ class ProductRepository extends ServiceEntityRepository
         parent::__construct($registry, Product::class);
     }
 
+    public function findPage(int $page = 1, int $limit = 25): array
+    {
+        return $this->findBy([], [], $limit, $limit * ($page - 1));
+    }
+
     public function findBySkuAndPrice(string $sku, float $minPrice, float $maxPrice)
     {
         $products = $this->createQueryBuilder('p')
