8a99e3066eea3010be70190f8fd46bce1579ba17
Jelmer <jelmer@ketelaar.me>
2021-12-01 18:38:37 +0100

    Script now checksif url is local or dev. And goes back to the one your on when clicking back button

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,4 +1,6 @@
-var baseUrl = 'https://sommelier.apps.grandcruwijnen.dev/';
+const urlPieces = [location.protocol, '//', location.host, '/']
+let url = urlPieces.join('');
+let baseUrl = url;
 
 function goBack(currentUrl) {
 
@@ -24,42 +26,35 @@ function goBack(currentUrl) {
 window.addEventListener("DOMContentLoaded", () => {
     const tabs = document.querySelectorAll('[role="tab"]');
     const tabList = document.querySelector('[role="tablist"]');
-  
+
     // Add a click event handler to each tab
     tabs.forEach(tab => {
-      tab.addEventListener("click", changeTabs);
+        tab.addEventListener("click", changeTabs);
     });
-  });
-  
-  function changeTabs(e) {
+});
+
+function changeTabs(e) {
     const target = e.target;
     const parent = target.parentNode;
     const grandparent = parent.parentNode;
-  
+
     // Remove all current selected tabs
     parent
-      .querySelectorAll('[aria-selected="true"]')
-      .forEach(t => t.setAttribute("aria-selected", false));
-  
+        .querySelectorAll('[aria-selected="true"]')
+        .forEach(t => t.setAttribute("aria-selected", false));
+
     // Set this tab as selected
     target.setAttribute("aria-selected", true);
-  
+
     // Hide all tab panels
     grandparent
-      .querySelectorAll('[role="tabpanel"]')
-      .forEach(p => p.setAttribute("hidden", true));
-  
+        .querySelectorAll('[role="tabpanel"]')
+        .forEach(p => p.setAttribute("hidden", true));
+
     // Show the selected panel
     grandparent.parentNode
-      .querySelector(`#${target.getAttribute("aria-controls")}`)
-      .removeAttribute("hidden");
-  }
+        .querySelector(`#${target.getAttribute("aria-controls")}`)
+        .removeAttribute("hidden");
+}
 
 // ---------------------- einde meer informatie tabs -----------------------------
-
-
-function choiceMade(actionLink, choice) {
-  alert(actionLink);
-  document.getElementById("page-description").innerHTML = choice;
-
-}
