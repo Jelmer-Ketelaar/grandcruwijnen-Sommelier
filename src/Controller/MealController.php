51793c0fbaa109fda73c6eaba7938a0db2c85f43
Jelmer Ketelaar <jelmer@ketelaar.me>
2021-12-30 22:54:08 +0100

    #101

--- src/Controller/MealController.php
+++ src/Controller/MealController.php
@@ -14,7 +14,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
-class MealController extends AbstractController {
+class MealController extends AbstractController
+{
 
     private $requestStack;
 
@@ -69,21 +70,19 @@ class MealController extends AbstractController {
         $matches = [];
 
         //If there is any letter in front it will convert to a 0.
-        $page = (int) $request->query->get('page');
+        $page = (int)$request->query->get('page');
 
         //set 18 products per page
         $productsPerPage = 18;
         //start at page 1 if no page is clicked yet
-        if ($page === 0)
-        {
+        if ($page === 0) {
             $page = 1;
         }
         //skuScores is an arry
         $skuScores = [];
         //skus is an arry
         $skus = [];
-        foreach ($mealMatcherService->getWinesForMeal($mealId) as $wine)
-        {
+        foreach ($mealMatcherService->getWinesForMeal($mealId) as $wine) {
             $skus[] = $wine->wine->sku;
             $skuScores[$wine->wine->sku] = $wine->score;
         }
@@ -94,12 +93,10 @@ class MealController extends AbstractController {
         $formMinPrice = $request->query->get('price-min');
         $formMaxPrice = $request->query->get('price-max');
 
-        if ($formMinPrice === null)
-        {
+        if ($formMinPrice === null) {
             $formMinPrice = 1;
         }
-        if ($formMaxPrice === null)
-        {
+        if ($formMaxPrice === null) {
             $formMaxPrice = 100000;
         }
 
@@ -108,9 +105,7 @@ class MealController extends AbstractController {
         $specialPriceSorts = $request->request->get('specialPriceSorts');
 
 
-
-        if ($specialPriceSorts !== null)
-        {
+        if ($specialPriceSorts !== null) {
             $specialPriceSubmit = true;
         } else {
             $specialPriceSubmit = false;
@@ -123,36 +118,32 @@ class MealController extends AbstractController {
         $products = $this->getDoctrine()->getRepository(Product::class)->findWinesBySkus($skus);
 
 
-        foreach ($products as $product)
-        {
+        foreach ($products as $product) {
             $productMatch = new ProductMatch($product, $skuScores[$product->getSku()]);
             $winePrice = $productMatch->product->getprice();
 
 
-            if ($minWinePrice > $winePrice)
-            {
+            if ($minWinePrice > $winePrice) {
                 $minWinePrice = $winePrice;
             }
 
-            if ($maxWinePrice < $winePrice)
-            {
+            if ($maxWinePrice < $winePrice) {
                 $maxWinePrice = $winePrice;
             }
 
-            if ($winePrice >= $formMinPrice && $winePrice <= $formMaxPrice)
-            {
+            if ($winePrice >= $formMinPrice && $winePrice <= $formMaxPrice) {
                 $matches[] = $productMatch;
             }
         }
 
         usort($matches, static function ($matchA, $matchB) {
-            return $matchA->getScore() === $matchB->getScore() ? 0 : ($matchA->getScore() < $matchB->getScore() ? 1 : - 1);
+            return $matchA->getScore() === $matchB->getScore() ? 0 : ($matchA->getScore() < $matchB->getScore() ? 1 : -1);
         });
 
         $matchesForPage = array_slice($matches, $productsPerPage * ($page - 1), $productsPerPage);
 
         $totalProductCount = count($matches);
-        $totalPages = (int) ceil($totalProductCount / $productsPerPage);
+        $totalPages = (int)ceil($totalProductCount / $productsPerPage);
 
         $mealArr = [urldecode($mealId)];
 
@@ -166,47 +157,35 @@ class MealController extends AbstractController {
         $countSpecialPrice = 0;
 
 
-        foreach ($matchesForPage as $amountWine)
-        {
-            if ($amountWine->product->getWineSort() === 'Wit')
-            {
-                $countWit ++;
+        foreach ($matchesForPage as $amountWine) {
+            if ($amountWine->product->getWineSort() === 'Wit') {
+                $countWit++;
             }
-            if ($amountWine->product->getWineSort() === 'Rood')
-            {
-                $countRood ++;
+            if ($amountWine->product->getWineSort() === 'Rood') {
+                $countRood++;
             }
-            if ($amountWine->product->getWineSort() === 'Rosé')
-            {
-                $countRosé ++;
+            if ($amountWine->product->getWineSort() === 'Rosé') {
+                $countRosé++;
             }
-            if ($amountWine->product->getWineSort() === 'Port')
-            {
-                $countPort ++;
+            if ($amountWine->product->getWineSort() === 'Port') {
+                $countPort++;
             }
-            if ($amountWine->product->getWineSort() === 'Sherry')
-            {
-                $countSherry ++;
+            if ($amountWine->product->getWineSort() === 'Sherry') {
+                $countSherry++;
             }
-            if ($amountWine->product->getWineSort() === 'Madeira')
-            {
-                $countMadeira ++;
+            if ($amountWine->product->getWineSort() === 'Madeira') {
+                $countMadeira++;
             }
-            if ($amountWine->product->getWineSort() === 'Vermout')
-            {
-                $countVermout ++;
+            if ($amountWine->product->getWineSort() === 'Vermout') {
+                $countVermout++;
             }
-            if ($amountWine->product->getSpecialPrice() !== null)
-            {
-                $countSpecialPrice ++;
+            if ($amountWine->product->getSpecialPrice() !== null) {
+                $countSpecialPrice++;
             }
         }
 
-
-
         $countWines = ['countWit' => $countWit, 'countRood' => $countRood, 'countRosé' => $countRosé, 'countPort' => $countPort, 'countSherry' => $countSherry, 'countMadeira' => $countMadeira, 'countVermout' => $countVermout, 'countSpecialPrice' => $countSpecialPrice];
 //        dd($countWines);
-
 
 
         return $this->render('wines/index.html.twig', [
